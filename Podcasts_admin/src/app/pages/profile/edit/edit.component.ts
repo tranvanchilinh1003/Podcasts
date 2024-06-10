82974a22dbdee7f7a3eb92600d60b49fa4d4a3d1
tranvanchilinh1003 <chilinh140220@gmail.com>
2024-06-10 20:25:03 +0700

    [ADMIN] chỉnh lại info thấy gớm của hiếu

--- Podcasts_admin/src/app/pages/profile/edit/edit.component.ts
+++ Podcasts_admin/src/app/pages/profile/edit/edit.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {LocalStorageService} from "../../../@core/services/common/local-storage.service";
+import {LOCALSTORAGE_KEY, ROUTER_CONFIG} from "../../../@core/config";
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent {
+  inforAdmin = this.storageService.getItem(LOCALSTORAGE_KEY.userInfo);
+  constructor(
+    private storageService: LocalStorageService
+  ){
 
+  }
+  OnInit(){
+    
+  }
+  
 }
--- Podcasts_admin/src/app/pages/profile/profile.component.ts
+++ Podcasts_admin/src/app/pages/profile/profile.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { LOCALSTORAGE_KEY } from 'app/@core/config/local-storage-key.config';
+import { LocalStorageService } from 'app/@core/services/common';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  inforAdmin = this.storageService.getItem(LOCALSTORAGE_KEY.userInfo);
+  constructor(
+    private storageService: LocalStorageService
+  ){
+
+  }
+}
